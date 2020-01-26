8c6734540de5b4498c3e6e0ed6e91b7a00ef1dd4
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-01-26 19:24:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,28 +2,24 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 const token = 'NjcwNjg1ODI3MTU1NjIzOTM3.Xix_qA.1OIvwCn9RKQmg_V18vcr8R2GkUI';
+const PREFIX = 'r.';
 
 bot.on('ready',()=>{
     console.log('Bot is online!')
 })
 
 bot.on('message', msg=>{
-    if(msg.content === "Hello"){
-    msg.reply('Hello there');
+
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+case 'ping':
+msg.reply ('pong!');
+break;
     }
 })
 
-bot.on('message', msg=>{
-    if(msg.content === "your gay"){
-    msg.reply('shut up your gay too');
-    }
-})
-
-bot.on('message', msg=>{
-    if(msg.content === "your lesbian"){
-    msg.reply('shut up your lesbian too');
-    }
-})
 
 bot.login(process.env.token);
+
 
