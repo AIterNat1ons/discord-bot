a384c13beeee989c75cfe8eba314351c79fef3f0
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-01-26 21:21:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bot = new Discord.Client();
 
 const token = 'NjcwNjg1ODI3MTU1NjIzOTM3.Xix_qA.1OIvwCn9RKQmg_V18vcr8R2GkUI';
 const PREFIX = 'r.';
-const attachment = Attachment('https://i.imgur.com/w3duR07.png');
+const attachment = ('https://i.imgur.com/w3duR07.png');
+
 
 bot.on('ready',()=>{
     console.log('Bot is online!')
