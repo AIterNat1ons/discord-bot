41da0f069d0a05f9b38944c58be4bd10b597b1b4
AIterNat1ons <60318095+AIterNat1ons@users.noreply.github.com>
2020-01-27 13:26:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ break;
                 msg.reply (msg.author.avatarURL);
             break;
         case 'rip':
-            const attachment =('https://i.imgur.com/w3duR07.png');
+            const attachment = new attachment ('https://i.imgur.com/w3duR07.png');
             msg.channel.sendMessage(attachment);
             break;
                 
